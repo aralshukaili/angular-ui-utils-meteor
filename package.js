6fa5ed10b933f47c8d0b2aacbc2a2ef022686d4d
Awadh Al Shukaili <aralshukaili@gmail.com>
2014-02-20 03:38:56 +1100

    first commit

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+	summary: "angular-utils-meteor"
+});
+
+Package.on_use(function (api) {
+	api.use('ngMeteor', 'client');
+	
+	// Files to load in Client only.
+	api.add_files([
+		// Lib Files
+		'angular-utils.js',
+		'ui-utils-ieshiv.js',
+		'angular-ui-meteor.js'
+	], 'client');
+});
